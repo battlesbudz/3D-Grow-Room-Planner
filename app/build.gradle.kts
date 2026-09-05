d56d2c7fe9ba1cc8e9daf81642ac55b0d04e1049
battlesbudz <battlesbudz@gmail.com>
2026-09-05 18:20:14 -0400

    Align Android Java and Kotlin JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 
 android { namespace = "com.battlesbudz.growroomplanner"; compileSdk = 36
     defaultConfig { applicationId = "com.battlesbudz.growroomplanner"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
